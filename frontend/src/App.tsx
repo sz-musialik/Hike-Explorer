b6ebc09fbab9b45b1a265c622a8215a13ee6364a
sz-musialik <280117@student.pwr.edu.pl>
2026-08-07 19:47:08 +0200

    Add OpenStreetMap using react-leaflet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,28 @@
 // import { useState } from 'react'
-import './App.css'
+import './App.css';
 import Navbar from './components/Navbar.tsx';
-import Map from './components/Map.tsx';
 import HikeInfo from './components/HikeInfo.tsx';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <div className='main-container'>
 			<Navbar/>
 
 			<div className='main-content-container'>
-
 				<div className='main-left'>
 					<div className='map-container'>
-						<Map/>
+						<MapContainer
+							center={[50.061, 19.937]}
+							zoom={13}
+							style={{ height: '100%', width: '100%' }}
+						>
+							<TileLayer
+								attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+								url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+							/>
+						</MapContainer>
 					</div>
 
 					<div className='hike-info-container'>
